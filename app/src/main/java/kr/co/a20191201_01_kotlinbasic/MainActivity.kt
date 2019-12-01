5ed8f5bb5b4397e81cd234a8d6f3c798d9c09b2e
CUdex9779 <cuyu9779@gmail.com>
2019-12-01 11:27:09 +0900

    Log출력을 연습하였다

--- app/src/main/java/kr/co/a20191201_01_kotlinbasic/MainActivity.kt
+++ app/src/main/java/kr/co/a20191201_01_kotlinbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.a20191201_01_kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,12 +18,17 @@ class MainActivity : AppCompatActivity() {
 
            var inputString =  inputEdt.text.toString() // get => 내용물 받아오기.
             resultTxt.text = inputString // set => 내용물 설정하기.
+
+//            확인버튼이 눌렸는지 로그
+            Log.d("확인 버튼 클릭",inputString)
         }
 
         okBtn.setOnLongClickListener {
 
 
             resultTxt.text = "초기상태"
+
+            Log.d("확인버튼롱클릭","텍스트뷰초기화")
 //
 //            return의 의미는 OnClick을 무시할건지 말건지
 //            true 일 경우 LongClick만 실행되지만
