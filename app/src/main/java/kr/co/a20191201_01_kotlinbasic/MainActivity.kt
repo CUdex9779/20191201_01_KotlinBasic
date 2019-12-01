07b94b2a3d91a484adcc43ac5e560e8cb46810d9
CUdex9779 <cuyu9779@gmail.com>
2019-12-01 10:25:20 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/a20191201_01_kotlinbasic/MainActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.a20191201_01_kotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        버튼이 눌리면
+//        EditText(inputEdt)에 적힌 값을 받아서 TextView(resultTxt)에 내용으로 반영
+
+        okBtn.setOnClickListener {
+            
+        }
+
+
+    }
+}
